8f0abb185e26d5dd66ffed7d381faef02a3dbb3c
gk07 <gk_520@hotmail.com>
2020-08-18 15:19:05 +0800

    Display gantt in nw.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const xlsxFile = require("read-excel-file/node");
-var Highcharts = require("highcharts");
+// var Highcharts = require("highcharts");
 // Load module after Highcharts is loaded
-require("highcharts/modules/gantt")(Highcharts);
+// require("highcharts/modules/gantt")(Highcharts);
 
 const fpath = "../schedule.xlsx";
+var datas = [];
 
-xlsxFile(fpath, { getSheets: true })
-  .then((sheets) => {
-    // sheets.forEach((obj) => {
-    //   console.log(obj.name);
-    // });
+xlsxFile(fpath, { getSheets: true }).then((sheets) => {
+  // sheets.forEach((obj) => {
+  //   console.log(obj.name);
+  // });
 
-    xlsxFile(fpath, { sheet: sheets[0].name }).then((rows) => {
-      // console.log(rows);
-      // console.log(rows[0]);
+  xlsxFile(fpath, { sheet: sheets[0].name }).then((rows) => {
+    console.log(rows);
+    console.log(rows[0]);
+    console.log(rows[1]);
+    console.log(rows[2]);
 
-      return formatData(rows);
-      // console.log(datas);
-    });
-  })
-  .then((datas) => {
+    var datas = formatData(rows);
+    console.log(datas);
+
     Highcharts.ganttChart("container", {
       title: {
         text: "综合布线施工进度计划",
@@ -32,21 +32,44 @@ xlsxFile(fpath, { getSheets: true })
         },
       ],
     });
+    return datas;
   });
+});
 
 function formatData(rows) {
-  let header = rows[0];
-  var datas = [];
+  var header = rows[0];
+  console.log(header);
+  // var datas = [];
 
   rows.slice(1).forEach((row) => {
     var data = {};
 
     for (i = 0; i < header.length; i++) {
-      // console.log(header[i]);
-      data[header[i]] = row[i];
+      let value = row[i];
+      if (value) {
+        if (header[i] == "start" || header[i] == "end") {
+          data[header[i]] = formatDate(value);
+        } else if (header[i] == "dependency") {
+          data[header[i]] = formatDep(value.toString());
+        } else if (header[i] == "id" || header[i] == "parent") {
+          if (typeof value != "string") {
+            data[header[i]] = value.toString();
+          }
+        } else {
+          data[header[i]] = value;
+        }
+      }
     }
     datas.push(data);
   });
 
   return datas;
+}
+
+function formatDate(date) {
+  return Date.UTC(date.getFullYear(), date.getMonth() + 1, date.getDate());
+}
+
+function formatDep(dependency) {
+  return dependency.replace(/，/gi, ",").split(",");
 }
