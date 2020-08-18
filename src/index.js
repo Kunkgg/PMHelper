a0a7a22a7b97bea41569303e1f06e05222344002
gk07 <gk_520@hotmail.com>
2020-08-18 11:50:08 +0800

    Can lanuch by nw, but Can't load Highcharts

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
 const xlsxFile = require("read-excel-file/node");
+var Highcharts = require("highcharts");
+// Load module after Highcharts is loaded
+require("highcharts/modules/gantt")(Highcharts);
 
 const fpath = "../schedule.xlsx";
 
 xlsxFile(fpath, { getSheets: true })
   .then((sheets) => {
-    sheets.forEach((obj) => {
-      console.log(obj.name);
-    });
+    // sheets.forEach((obj) => {
+    //   console.log(obj.name);
+    // });
 
-    return sheets[0].name;
+    xlsxFile(fpath, { sheet: sheets[0].name }).then((rows) => {
+      // console.log(rows);
+      // console.log(rows[0]);
+
+      return formatData(rows);
+      // console.log(datas);
+    });
   })
-  .then((sheet) => {
-    xlsxFile(fpath, { sheet: sheet }).then((rows) => {
-      console.log(rows);
+  .then((datas) => {
+    Highcharts.ganttChart("container", {
+      title: {
+        text: "综合布线施工进度计划",
+      },
+
+      series: [
+        {
+          name: "三星工厂综合布线",
+          data: datas,
+        },
+      ],
     });
   });
 
-// xlsxFile("./Data.xlsx", { sheet: "Exec" }).then((rows) => {
-//   for (i in rows) {
-//     for (j in rows[i]) {
-//       console.log(rows[i][j]);
-//     }
-//   }
-// });
+function formatData(rows) {
+  let header = rows[0];
+  var datas = [];
+
+  rows.slice(1).forEach((row) => {
+    var data = {};
+
+    for (i = 0; i < header.length; i++) {
+      // console.log(header[i]);
+      data[header[i]] = row[i];
+    }
+    datas.push(data);
+  });
+
+  return datas;
+}
